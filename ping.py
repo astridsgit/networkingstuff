81d2aa68c48206a0a8013002812d6a0da19c9f2e
astrid <azzyahya83@gmail.com>
2026-07-31 02:25:33 +0100

    removed spaces

--- ping.py
+++ ping.py
@@ -5,26 +5,20 @@ subnet = "192.168.1."
 
 print("Starting upgraded network sweep...")
 
+
 for i in range(1, 51):
     ip_address = subnet + str(i)
-
-
     response = subprocess.call(
         ['ping', '-c', '1', '-W', '1', ip_address],
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL
     )
-
     if response == 0:
         mac_address = "Unknown MAC"
         hostname = "Unknown Name"
-
-
         try:
-
             hostname = socket.gethostbyaddr(ip_address)[0]
         except socket.herror:
-
             pass
 
         arp_result = subprocess.run(
@@ -33,10 +27,7 @@ for i in range(1, 51):
             text=True
         )
         output_words = arp_result.stdout.split()
-
         if 'lladdr' in output_words:
-
             mac_index = output_words.index('lladdr') + 1
             mac_address = output_words[mac_index]
-
         print(f"[+] IP: {ip_address} | Name: {hostname} | MAC: {mac_address}")
